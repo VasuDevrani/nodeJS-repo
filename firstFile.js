a4b63c4b9269498ee45b9ac8e0b51f68846552ba
VasuDevrani <vasu2002devrani@gmail.com>
2022-06-24 12:05:23 +0530

    nodeJS files

---
+++ firstFile.js
@@ -0,0 +1,9 @@
+let count = 10;
+
+if(count > 22){
+    console.log("large number")
+}
+else{
+    console.log("smaller one")
+}
+console.log("my first app")
---
+++ globalVar.js
@@ -0,0 +1,18 @@
+// GLOBALS - no window
+// These are the variables or functions that are accessible thorughout the program
+// ---some example----
+
+
+// __dirname - path of current directory
+// __filename - filename
+// require - function to use modules(CommonJs)
+// module - info about current module(file)
+// process - info about environment where the program is currently executed
+
+console.log(__dirname);
+console.log(__filename);
+console.log(process);
+
+setInterval(() => {
+    console.log('nodeJS')
+}, 1000)
